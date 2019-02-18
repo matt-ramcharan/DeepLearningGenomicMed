b7028a36b78f3c5125100fec8db1d7c6eb59ef9c
matt-ramcharan <matthew.a.ramcharan@gmail.com>
2019-02-18 22:37:38 +0000

    Tree Generation working now. Creates correct leaves, need to extract to dataset

--- Python/MultitaskLearn/DoingItLive.py
+++ Python/MultitaskLearn/DoingItLive.py
@@ -11,30 +11,26 @@ import matplotlib.pyplot as plt
 def similarity_func(u, v):
     return 1/(1+euclidean(u,v))
 
-"""
+
 def change_repres_rand(top,node,m):
-    #print(top.represN)
-    top[node].pprint()
-    #top[node].represN = [1,1,1]
-    subtree = top[node].levelorder
-    for i in subtree:
-        repres_index = np.random.choice(range(len(i.represN)), m, replace=False)
+    #top[node].pprint()
+    subtree = top[node].values
+    repres_index = np.random.choice(range(len(top[node].represN)), m, replace=False)
+    for subnode in subtree:
+        cur_rep = getattr(top[subnode], 'represN')
+        new_rep = cur_rep
         for j in repres_index:
-            cur_rep = getattr(i,'represN')
-            new_rep = cur_rep
             new_rep[j] = cur_rep[j]*-1
-            setattr(i,'represN',new_rep)
 
-            #setattr(i,'value',100)
-
-            print(getattr(i,'represN'))
-        print('\n')
+        setattr(top[subnode], 'represN', new_rep)
+        #print(getattr(top[subnode],'represN'))
+        #print('\n')
     return top
-"""
 def rand_change_recurse(root,m):
     order = root.preorder
     for node in order:
-        root = change_repres_rand(node,m)
+        root = change_repres_rand(root, node.value, m)
+        #print([leav.represN for leav in root.leaves])
     return root
 
 
@@ -42,41 +38,25 @@ if __name__ == "__main__":
 
     # Build a tree from list representation
 
-    tree_size = 7
-    #tree_size = 64-1
-    repres_size = 5
-    #repres_size = 100
+    #tree_size = 7
+    tree_size = 64-1
+    #repres_size = 5
+    repres_size = 100
 
 
     values = range(0,tree_size)
     repres_orig = [[1]*repres_size] * tree_size
     root = build(values,repres=repres_orig.copy())
-    print(repres_orig)
+    #print(repres_orig)
 
-    m = 3
+    #m = 5
+    m=1
 
     root.pprint()
+    #root = change_repres_rand(root, 1, m)
+    root = rand_change_recurse(root, m)
 
-    node = 1
-    root[node].pprint()
-    #top[node].represN = [1,1,1]
-    subtree = root[node].values
-
-    for subnode in subtree:
-
-        repres_index = np.random.choice(range(len(root[subnode].represN)), m, replace=False)
-        cur_rep = getattr(root[subnode], 'represN')
-        new_rep = cur_rep
-        for j in repres_index:
-            new_rep[j] = cur_rep[j]*-1
-
-        setattr(root[subnode], 'represN', new_rep)
-        print(getattr(root[subnode],'represN'))
-        print('\n')
-
-
-    print(repres_orig)
-    root.pprint()
+    #root.pprint()
     represes = [rep.represN for rep in root.leaves]
     #np.array(represes).T.tolist()
     labels = root.leaves
@@ -86,6 +66,6 @@ if __name__ == "__main__":
 
     dists = pdist(DF_var, similarity_func)
     DF_euclid = pd.DataFrame(squareform(dists))
-    print(DF_euclid)
+    #print(DF_euclid)
     plt.matshow(DF_euclid.corr())
     plt.show()
