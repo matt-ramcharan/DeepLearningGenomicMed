4d78a78b937b77e3dda67c628b3138d66cc660be
matt-ramcharan <matthew.a.ramcharan@gmail.com>
2019-03-12 12:42:57 +0000

    Shogun SVM Gaussian Kernel with tree data working

--- Python/SupportVectorMachine/SVMShogun.py
+++ Python/SupportVectorMachine/SVMShogun.py
@@ -1,30 +1,45 @@
+# import numpy as np
+# import shogun
+# from shogun.Features import LibSVM
+#from shogun.Kernel import *
+# from shogun.Classifier import AccuracyMeasure
+# from shogun.Evaluation import RealFeatures, BinaryLabels, AccuracyMeasure
+# from shogun.Kernel import GaussianKernel
+from shogun.Evaluation import RealFeatures, BinaryLabels, LibSVM, AccuracyMeasure
 import numpy as np
-import shogun
-from shogun.Features import *
-from shogun.Kernel import *
-from shogun.Classifier import *
-from shogun.Evaluation import *
+from numpy.random import randn
+from shogun.Kernel import GaussianKernel
+
 
 train_data = np.load('/home/matt/Documents/TechnicalProject/DeepLearningGenomicMed/Python/MultitaskLearn/train.npy')
 test_data = np.load('/home/matt/Documents/TechnicalProject/DeepLearningGenomicMed/Python/MultitaskLearn/test.npy')
 
-features_train = RealFeatures(train_data[1, :, :-1])
-features_test = RealFeatures(test_data[1, :, :-1])
-labels_train = BinaryLabels(train_data[1, :, -1])
-labels_test = BinaryLabels(test_data[1, :, -1])
+train_feats = train_data[1, :, :-1]
+test_feats = test_data[1, :, :-1]
+train_label = train_data[1, :, -1]
+test_label = test_data[1, :, -1]
 
-C = 1.0
+features_train = RealFeatures(train_feats.T)
+features_test = RealFeatures(test_feats.T)
+labels_train = BinaryLabels(train_label.T)
+labels_test = BinaryLabels(test_label.T)
 epsilon = 0.001
-gauss_kernel = GaussianKernel(features_train, features_train, 15)
+C = 1.0
+#
+gauss_kernel = GaussianKernel(features_train, features_train, 2)
 
 svm = LibSVM(C, gauss_kernel, labels_train)
-svm.set_epsilon(epsilon)
+#svm.set_epsilon(epsilon)
 
 svm.train()
+
+
 labels_predict = svm.apply_binary(features_test)
 
 alphas = svm.get_alphas()
 b = svm.get_bias()
 
 eval = AccuracyMeasure()
+
 accuracy = eval.evaluate(labels_predict, labels_test)
+print(accuracy)
